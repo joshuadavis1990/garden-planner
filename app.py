27d1d60a6fef50ddf55cf4fddd0fc298f9b300ec
joshuadavis1990 <joshuadavis1990@me.com>
2023-06-20 13:37:49 +1000

    Add basic User, Area, PlantRecord, Plant and Space models

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://garden_planner_dev:camellia@localhost:5432'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    f_name = db.Column(db.String(100))
+    l_name = db.Column(db.String(100))
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(100))
+
+class Area(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class PlantRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class Plant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class Space(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
