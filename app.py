1d89708e9828e13a7689381ebbbf6ff3ef8601f3
joshuadavis1990 <joshuadavis1990@me.com>
2023-06-20 20:05:29 +1000

    Add seed CLI command

--- app.py
+++ app.py
@@ -8,28 +8,33 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://garden_planner_de
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     f_name = db.Column(db.String(100))
     l_name = db.Column(db.String(100))
     email = db.Column(db.String(50))
     password = db.Column(db.String(100))
 
-class Area(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-class PlantRecord(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-class Plant(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-class Space(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
 @app.cli.command('create')
 def create_db():
+    db.drop_all()
     db.create_all()
     print('Tables created successfully')
+
+@app.cli.command('seed')
+def seed_db():
+    user = User(
+        f_name = 'Anakin',
+        l_name = 'Skywalker',
+        email = 'anakin@skywalker.com',
+        password = 'darthvader'
+    )
+
+    db.session.query(User).delete()
+    db.session.add(user)
+    db.session.commit()
+    print('Models seeded')
 
 @app.route('/')
 def index():
