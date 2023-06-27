3f4757e08846fcde13cf02bcb5ba9255fd7dfb98
joshuadavis1990 <joshuadavis1990@me.com>
2023-06-27 13:14:05 +1000

    Refactor app.py to use flask command

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from os import environ
+from init import db, ma, bcrypt, jwt
+from blueprints.cli_bp import cli_bp
+from blueprints.auth_bp import auth_bp
+from blueprints.plantrecords_bp import plantrecords_bp
+
+# Factory function for creating and configuring an object, 'app', and returning it
+def setup():
+    app = Flask(__name__)
+
+    app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URI')
+
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    @app.errorhandler(401)
+    def unauthorized(err):
+        return {'error': 'You must be an admin'}, 401
+            
+    app.register_blueprint(cli_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(plantrecords_bp)
+
+    return app
