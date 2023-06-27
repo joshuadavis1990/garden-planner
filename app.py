7d74c7ad831289aa358e0a8dc177f3ef21a6e17a
joshuadavis1990 <joshuadavis1990@me.com>
2023-06-27 11:56:07 +1000

    Refactor auth_bp blueprint

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 from models.user import User, UserSchema
 from models.plantrecord import PlantRecord, PlantRecordSchema
 from init import db, ma, bcrypt, jwt
-from blueprints.cli_bp import db_commands
+from blueprints.cli_bp import cli_bp
+from blueprints.auth_bp import auth_bp
 
 load_dotenv()
 
@@ -32,42 +33,9 @@ def admin_required():
 def unauthorized(err):
     return {'error': 'You must be an admin'}, 401
         
-app.register_blueprint(db_commands)
+app.register_blueprint(cli_bp)
+app.register_blueprint(auth_bp)
 
-@app.route('/register', methods = ['POST'])
-def register():
-    try:
-        # Parse, sanitise and validate the incoming JSON data via the schema
-        user_info = UserSchema().load(request.json)
-        # Create a new User model instance with the schema data
-        user = User(
-            f_name = user_info['f_name'],
-            l_name = user_info['l_name'],
-            email = user_info['email'],
-            password = bcrypt.generate_password_hash(user_info['password']).decode('utf-8')
-        )
-        
-        # Add and commit the new user
-        db.session.add(user)
-        db.session.commit()
-
-        # Return the new user to the client, excluding the password
-        return UserSchema(exclude=['password']).dump(user), 201
-    except IntegrityError:
-        return {'error': 'Email address already in use'}, 409
-    
-@app.route('/login', methods=['POST'])
-def login():
-    try:
-        stmt = db.select(User).filter_by(email=request.json['email'])
-        user = db.session.scalar(stmt)
-        if user and bcrypt.check_password_hash(user.password, request.json['password']):
-            token = create_access_token(identity=user.email, expires_delta=timedelta(days=1))
-            return {'token': token, 'user': UserSchema(exclude=['password']).dump(user)}
-        else:
-            return {'error': 'Invalid email address or password'}, 401
-    except KeyError:
-        return {'error': 'Email address and password are required'}, 400
 
 @app.route('/plantrecords')
 @jwt_required()
--- blueprints/cli_bp.py
+++ blueprints/cli_bp.py
@@ -3,15 +3,15 @@ from models.user import User
 from models.plantrecord import PlantRecord
 from init import db, bcrypt
 
-db_commands = Blueprint('db', __name__)
+cli_bp = Blueprint('db', __name__)
 
-@db_commands.cli.command('create')
+@cli_bp.cli.command('create')
 def create_db():
     db.drop_all()
     db.create_all()
     print('Tables created successfully')
 
-@db_commands.cli.command('seed')
+@cli_bp.cli.command('seed')
 def seed_db():
     # Create separate instances of the User model in memory
     users = [
