7c6eeb8d7228b3039754527cf39a9e08ad4b1271
joshuadavis1990 <joshuadavis1990@me.com>
2023-06-27 11:35:22 +1000

    Refactor cli_bp blueprint

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from models.user import User, UserSchema
 from models.plantrecord import PlantRecord, PlantRecordSchema
 from init import db, ma, bcrypt, jwt
+from blueprints.cli_bp import db_commands
 
 load_dotenv()
 
@@ -31,67 +32,7 @@ def admin_required():
 def unauthorized(err):
     return {'error': 'You must be an admin'}, 401
         
-@app.cli.command('create')
-def create_db():
-    db.drop_all()
-    db.create_all()
-    print('Tables created successfully')
-
-@app.cli.command('seed')
-def seed_db():
-    # Create separate instances of the User model in memory
-    users = [
-        User(
-            f_name = 'Joshua',
-            l_name = 'Davis',
-            email = '14209@coderacademy.edu.au',
-            password = bcrypt.generate_password_hash('coderacademy').decode('utf-8'),
-            is_admin = True
-        ),
-        User(
-            f_name = 'Neil',
-            l_name = 'Armstrong',
-            email = 'neilarmstrong@gmail.com',
-            password = bcrypt.generate_password_hash('astronaut').decode('utf-8')
-        ),
-        User(
-            f_name = 'Donald',
-            l_name = 'Trump',
-            email = 'donaldtrump@gmail.com',
-            password = bcrypt.generate_password_hash('makeamericagreat').decode('utf-8')
-        )
-    ]
-    # Create separate instances of the PlantRecords model in memory
-    plant_records = [
-        PlantRecord(
-            name = 'Camellia',
-            description = 'Camellias are attrative evergreen shrubs with a variety of flower colours.',
-            preferred_location = 'Partial sun',
-            water_rate = 'Average',
-            fertilisation_rate = 'Key feeding times are autumn as buds are developing and in spring once flowering has finished.',
-            other_comments = 'Keep the plant moist but well-drained.'
-        ),
-        PlantRecord(
-            name = 'Pansy',
-            description = 'A large-flowered hybrid plant cultivated as a garden flower.',
-            preferred_location = 'Partial sun',
-            water_rate = 'Average',
-            fertilisation_rate = 'Use a controlled release fertiliser when planting.',
-            other_comments = 'Keep the plant moist but well-drained.'
-        )
-    ]
-
-    # Truncate the tables
-    db.session.query(User).delete()
-    db.session.query(PlantRecord).delete()
-
-    # Add each user to the session (transaction)
-    db.session.add_all(users)
-    db.session.add_all(plant_records)
-
-    # Commit the users to the database
-    db.session.commit()
-    print('Models seeded')
+app.register_blueprint(db_commands)
 
 @app.route('/register', methods = ['POST'])
 def register():
---
+++ blueprints/cli_bp.py
@@ -0,0 +1,68 @@
+from flask import Blueprint
+from models.user import User
+from models.plantrecord import PlantRecord
+from init import db, bcrypt
+
+db_commands = Blueprint('db', __name__)
+
+@db_commands.cli.command('create')
+def create_db():
+    db.drop_all()
+    db.create_all()
+    print('Tables created successfully')
+
+@db_commands.cli.command('seed')
+def seed_db():
+    # Create separate instances of the User model in memory
+    users = [
+        User(
+            f_name = 'Joshua',
+            l_name = 'Davis',
+            email = '14209@coderacademy.edu.au',
+            password = bcrypt.generate_password_hash('coderacademy').decode('utf-8'),
+            is_admin = True
+        ),
+        User(
+            f_name = 'Neil',
+            l_name = 'Armstrong',
+            email = 'neilarmstrong@gmail.com',
+            password = bcrypt.generate_password_hash('astronaut').decode('utf-8')
+        ),
+        User(
+            f_name = 'Donald',
+            l_name = 'Trump',
+            email = 'donaldtrump@gmail.com',
+            password = bcrypt.generate_password_hash('makeamericagreat').decode('utf-8')
+        )
+    ]
+    # Create separate instances of the PlantRecords model in memory
+    plant_records = [
+        PlantRecord(
+            name = 'Camellia',
+            description = 'Camellias are attrative evergreen shrubs with a variety of flower colours.',
+            preferred_location = 'Partial sun',
+            water_rate = 'Average',
+            fertilisation_rate = 'Key feeding times are autumn as buds are developing and in spring once flowering has finished.',
+            other_comments = 'Keep the plant moist but well-drained.'
+        ),
+        PlantRecord(
+            name = 'Pansy',
+            description = 'A large-flowered hybrid plant cultivated as a garden flower.',
+            preferred_location = 'Partial sun',
+            water_rate = 'Average',
+            fertilisation_rate = 'Use a controlled release fertiliser when planting.',
+            other_comments = 'Keep the plant moist but well-drained.'
+        )
+    ]
+
+    # Truncate the tables
+    db.session.query(User).delete()
+    db.session.query(PlantRecord).delete()
+
+    # Add each user to the session (transaction)
+    db.session.add_all(users)
+    db.session.add_all(plant_records)
+
+    # Commit the users to the database
+    db.session.commit()
+    print('Models seeded')
